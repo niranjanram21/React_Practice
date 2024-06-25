e576215a0c82eb419d5eb77cc08b88fa47853122
Niranjan788 <your-niranjanram3657@gmail.com>
2024-06-25 10:52:52 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-// import MouseOver from './MouseOver'
-// import Interval from './setInterval'
-import FetchData from './FetchData'
+
+import ReducerCounter from './reducerCounter'
 
 const App = () => {
   return (
     <div>
-      {/* <MouseOver /> */}
-      {/* <Interval /> */}
-      <FetchData />
-      
+      <ReducerCounter />
     </div>
   )
 }
--- src/MouseOver.jsx
+++ src/MouseOver.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useEffect, useState } from 'react';
-
+ 
 function MouseOver() {
     const [x, setX] = useState(0);
     const [y, setY] = useState(0);
